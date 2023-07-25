d003539b8d7464d7a400c768a49b00f0e6f77fe8
martex941 <martex1997@gmail.com>
2023-07-25 09:33:01 +0200

    first commit

---
+++ network/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_creator", default="")
+    body = models.CharField(max_length=250)
+    timestamp = models.DateTimeField(default=timezone.now, editable=False)
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"A post by {self.creator} at {self.timestamp}"
+    
+class Like(models.Model):
+    liker = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liker", default="")
+    liked_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="liked_post", default="")
+
+    def __str__(self):
+        return f"{self.liker} liked {self.liked_post}"
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower", default="")
+    followee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followee", default="")
